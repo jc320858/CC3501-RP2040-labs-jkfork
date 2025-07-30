d10bc4097e326cecca3585302776faf692c9926d
jc320858 <jordan.kennett@my.jcu.edu.au>
2025-07-30 11:02:59 +1000

    ultrasonic commit

---
+++ src/drivers/ultrasonic.h
@@ -0,0 +1,7 @@
+#pragma once
+
+uint16_t read_distance_mm();
+
+void ultra_init();
+
+void run_ultrasonic();
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "hardware/pio.h"
 #include "drivers/loadcell.h"
 #include "drivers/IR.h"
+#include "drivers/ultrasonic.h"
 
 #include "WS2812.pio.h" // This header file gets produced during compilation from the WS2812.pio file
 #include "drivers/logging/logging.h"
